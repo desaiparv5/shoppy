f965507c106da0983c1fae692c0494042cc30ee7
shivam <shiv2299@gmail.com>
2020-02-25 15:34:22 +0530

    navigation and auth

---
+++ lib/BaseAuth.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+abstract class BaseAuth {
+  Future<String> signIn(String email, String password);
+
+  Future<String> signUp(String email, String password);
+
+  Future<FirebaseUser> getCurrentUser();
+
+  Future<void> sendEmailVerification();
+
+  Future<void> signOut();
+
+  Future<bool> isEmailVerified();
+}
+
+class Auth implements BaseAuth {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Future<String> signUp(String email, String password) async {
+    AuthResult authResult = await firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    FirebaseUser user = authResult.user;
+    return user.uid;
+  }
+
+  @override
+  Future<String> signIn(String email, String password) async {
+    AuthResult authResult = await firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    FirebaseUser user = authResult.user;
+    return user.uid;
+  }
+
+  @override
+  Future<void> signOut() {
+    return firebaseAuth.signOut();
+  }
+
+  @override
+  Future<FirebaseUser> getCurrentUser() async {
+    // TODO: implement getCurrentUser
+    FirebaseUser user = await firebaseAuth.currentUser();
+    return user;
+  }
+
+  @override
+  Future<bool> isEmailVerified() async {
+    FirebaseUser user = await firebaseAuth.currentUser();
+    return user.isEmailVerified;
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    FirebaseUser user = await firebaseAuth.currentUser();
+    user.sendEmailVerification();
+  }
+}
